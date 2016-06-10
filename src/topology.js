a7655cebd8414e6380bd7e97be5ba733b1704347
Julien Duponchelle <julien@duponchelle.info>
2016-06-10 14:01:07 +0200

    Show link

--- src/topology.js
+++ src/topology.js
@@ -2,5 +2,6 @@ import {bindable, bindingMode} from 'aurelia-framework';
 
 export class Topology {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) nodes;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) links;
 
 }
